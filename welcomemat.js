2380f48a633fc98253a2dc689d776f18bdcdd09c
palisadecoronet <landwaster301@gmail.com>
2018-10-02 21:17:31 -0700

    Add files via upload

--- welcomemat.js
+++ welcomemat.js
@@ -7,8 +7,6 @@
 	var soundLoop = false;
 	var soundLocal = false;
 	var soundVolume = 1.0;
-	var refreshInterval = 100;
-	var soundData = null;
 	var injector = null;
 	var entityID = null;
 	var properties = null;	
@@ -26,36 +24,37 @@
     this.enterEntity = function(pEntityID) {	
 		//var Me = AccountServices.username;
         entityID = pEntityID;
-		var intervalID = Script.setInterval(function() {		
-			properties = Entities.getEntityProperties(entityID, ["position"]); 
-				try {
-					soundURL = SoundCache.getSound(SOUND_URL);
-					refreshInterval = Math.min(1000, Math.max(refreshInterval, 10)); // cap updates at min 10 ms
-                } catch (e){}
+		properties = Entities.getEntityProperties(entityID, ["position"]); 
+		try {
+			soundURL = SoundCache.getSound(SOUND_URL);
+		} catch (e){}
 				
-			if (!injector) {
-				if (soundURL.downloaded) {
-					injector = Audio.playSound(soundURL, {
-						position: properties.position,
-						volume: soundVolume,
-						loop: soundLoop,
-						localOnly: soundLocal
-					});
-				}
-				lastSoundURL = soundData.soundURL;
-			} else {
-				if (lastSoundURL != soundData.soundURL) {
-					injector.stop();
-					injector = null;
+		if (!injector) {
+			if (soundURL.downloaded) {
+				Script.setTimeout(function () {
+				  injector = Audio.playSound(soundURL, {
+					position: properties.position,
+					volume: soundVolume,
+					loop: soundLoop,
+					localOnly: soundLocal
+				 });
+				}, 5000);
+			}
+			lastSoundURL = soundURL;
+		} else {
+			if (lastSoundURL != soundURL) {
+				injector.stop();
+				injector = null;
+				try {
 					soundURL = SoundCache.getSound(lastSoundURL);
-				} else {
+				} catch (e){}
+			} else {
 				injector.setOptions({
 					position: properties.position,
 					volume: soundVolume
 				});
 			}
-			}
-		}, refreshInterval);
+		}
 	};
  })
  
